bc340bd7e32d0e26c824bf1295c864955dc8e4cc
NicolasDestito <eduardozirbell26@gmail.com>
2021-09-24 18:39:33 -0300

    melhorando o javascript

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // pegar componente por ID
 const transactionsUl = document.querySelector('#transactions');
+const moneyPlusDisplay = document.querySelector('#money-plus');
+const moneyMinusDisplay = document.querySelector('#money-minus');
+const balanceDisplay = document.querySelector('#balance');
+const inputTransactionName = document.querySelector('#text');
+const inputTransactionAmount = document.querySelector('#amount');
 // const transactionsUl = document.querySelector('#balance')
 
 // declaracao de um objeto literal
@@ -11,8 +16,8 @@ const dummyTransactions = [
 ]
 
 const addTransactionIntoDom = transaction => {
-    const operator = transaction.amount < 0 ? '-' : '+';
-    const amountWithoutOperator = Math.abs(transaction.amount);
+    const operator = transaction.amount < 0 ? '-' : '+'; // pega o valor
+    const amountWithoutOperator = Math.abs(transaction.amount); // deixa o valor absoluto pro - ou + ficar no antes do R$
     const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
     const li = document.createElement('li');
     li.classList.add(CSSClass);
@@ -42,17 +47,25 @@ const updateBalanceValues = () => {
         .reduce((acumulator, transaction) => acumulator + transaction, 0)
         .toFixed(2);
 
-    console.log(transactionAmounts)
-    console.log("valor do reduce" + total);
+    
 
-    const income = transactionAmounts.filter(values = values > 0 ).reduce((acumulator, values) => acumulator + values, 0).toFixed(2);
+    // retorna todas as receitas
+    const income = transactionAmounts.filter(values => values > 0 ).reduce((acumulator, values) => acumulator + values, 0).toFixed(2);
 
+    // retorna todas as despesas
+    const expense = transactionAmounts
+    .filter(values => values < 0 )
+    .reduce((acumulator, values) => acumulator - values, 0)
+    .toFixed(2);
 
+    balanceDisplay.textContent = `R$ ${total}`
+    moneyPlusDisplay.textContent = `R$ ${income}`
+    moneyMinusDisplay.textContent = `R$ - ${expense}`
     
 
 }
 
-const init = () => {
+const init = () => { // inicializa todas as funções da pagina
     dummyTransactions.forEach(addTransactionIntoDom);
     updateBalanceValues();
 }
